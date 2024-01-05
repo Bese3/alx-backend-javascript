52373a2eb9bf8a61d7cd95e963528f7079007af2
besufikad <07yilmabese@gmail.com>
2024-01-05 17:58:31 +0300

    t 7

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,9 +1,8 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.any(
+  return Promise.race(
     [
       chinaDownload,
       USDownload,
     ],
-  )
-    .then((res) => res);
+  ).then((res) => res);
 }
