fc50e5f533e24f95976c583fd29a08b2c66f8de4
besufikad <07yilmabese@gmail.com>
2024-01-07 14:10:23 +0300

    t 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,28 +1,39 @@
-/* eslint-disable no-underscore-dangle */
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    } else {
+      // eslint-disable-next-line no-underscore-dangle
+      this._amount = amount;
+    }
+
+    // eslint-disable-next-line no-underscore-dangle
     this._currency = currency;
   }
 
   get amount() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._amount;
   }
 
   set amount(amount) {
+    // eslint-disable-next-line no-underscore-dangle
     this._amount = amount;
   }
 
   get currency() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._currency;
   }
 
   set currency(currency) {
+    // eslint-disable-next-line no-underscore-dangle
     this._currency = currency;
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+    // eslint-disable-next-line no-underscore-dangle
+    return `${this._amount} ${this._currency._name} ${this._currency._code}`;
   }
 
   static convertPrice(amount, conversionRate) {
