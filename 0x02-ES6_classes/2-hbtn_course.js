26b41d1e481659d745582ed84d7d415396c7ce7f
besufikad <07yilmabese@gmail.com>
2024-01-07 11:59:26 +0300

    t 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,47 +1,64 @@
-/* eslint-disable no-underscore-dangle */
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      // eslint-disable-next-line no-underscore-dangle
+      this._name = name;
+    }
     if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
+      throw new TypeError('length must be a number');
+    } else {
+      // eslint-disable-next-line no-underscore-dangle
+      this._length = length;
     }
     if (typeof students !== 'object') {
-      throw TypeError('Students must be an object');
+      throw new TypeError('students must be array');
+    } else {
+      // eslint-disable-next-line no-underscore-dangle
+      this._students = students;
     }
-
-    this._name = name;
-    this._length = length;
-    this._students = students;
   }
 
   get name() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._name;
   }
 
   get length() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._length;
   }
 
   get students() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._students;
   }
 
   set name(name) {
-    if (typeof name !== 'string') throw new TypeError('Name must be a string');
-    this._name = name;
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      // eslint-disable-next-line no-underscore-dangle
+      this._name = name;
+    }
   }
 
   set length(length) {
     if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
+      throw new TypeError('length must be a number');
+    } else {
+      // eslint-disable-next-line no-underscore-dangle
+      this._length = length;
     }
-    this._length = length;
   }
 
   set students(students) {
     if (typeof students !== 'object') {
-      throw TypeError('Students must be an object');
+      throw new TypeError('students must be array');
+    } else {
+      // eslint-disable-next-line no-underscore-dangle
+      this._students = students;
     }
-    this._students = students;
   }
 }
