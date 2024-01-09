998c5b1e823301278e8c16b15f194827af241a36
besufikad <07yilmabese@gmail.com>
2024-01-09 16:38:40 +0300

    t 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(map) {
+  if (!map instanceof Map) {
+    throw new Error('Cannot process')
+  }
   for (const k of map.keys()) {
     if (map.get(k) === 1) {
       map.set(k, 100);
