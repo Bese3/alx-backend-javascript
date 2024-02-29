740489f3be2f28501bc31bedf2c934cda33a5ef6
besufikad <07yilmabese@gmail.com>
2024-02-29 13:11:19 +0300

    t 0

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 module.exports = function displayMessage(str) {
-  process.stdout.write(`${str}\n`);
+  process.stdout.write(str + '\n');
 };
--- 0x05-Node_JS_basic/0-main.js
+++ 0x05-Node_JS_basic/0-main.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 const displayMessage = require('./0-console');
 
+
 displayMessage("Hello NodeJS!");
